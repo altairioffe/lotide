825d701bb07e1ddfa3f7fa7438f4c8c6cc87ccdd
Altair Ioffe <altair.ioffe@gmail.com>
2020-02-03 21:01:48 +0000

    linted assert equal function

---
+++ assertEqual.js
@@ -0,0 +1,7 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`) : console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
