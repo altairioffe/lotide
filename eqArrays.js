9579148b769748ac16e35600b02a70f317c1a213
Altair Ioffe <altair.ioffe@gmail.com>
2020-02-04 21:47:04 +0000

    equal arrays function added & tested

---
+++ eqArrays.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`) : console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function(arr1, arr2) {
+  let str1 = arr1.toString();
+  let str2 = arr2.toString();
+  let match = true;
+  for (let i = 0; i < str1.length; i++) {
+    str1[i] !== str2[i] ? match = false : ''
+  }
+  return match;
+};
+
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], ['one', 2, 3]), true); // => should FAIL
