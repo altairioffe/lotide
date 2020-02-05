b9b7fce8352a3813b713e457821d4f1d3c8a2cdf
Altair Ioffe <altair.ioffe@gmail.com>
2020-02-05 19:21:07 +0000

    functional code, tested

---
+++ finKeyByValue.js
@@ -0,0 +1,19 @@
+const assertEqual = function(actual, expected) {
+  actual === expected ? console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`) : console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(obj, value) {
+  let result = undefined;
+  for (let key in obj) {
+    obj[key] === value ? result = key : '';
+  }
+   return result;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
