1a7a212ac108b082b5c1282541b3f34ab85dada7
Altair Ioffe <altair.ioffe@gmail.com>
2020-02-05 18:55:46 +0000

    functional

---
+++ letterPositions.js
@@ -0,0 +1,22 @@
+const eqArrays = function (arr1, arr2) {
+  let match = true;
+  for (let i = 0; i < arr1.toString().length; i++) {
+    arr1.toString()[i] !== arr2.toString()[i] ? match = false : '';
+  }
+  return match;
+};
+
+const assertArraysEqual = function(actual, expected) {
+  actual === expected ? console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`) : console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const letterPositions = function(sentence) {
+  let results = {};
+  let arr = Array.from(sentence) // uncomment to filter out spaces // .filter(x => x !== ' ');
+  for (let i = 0; i < arr.length; i++) {
+    !results[arr[i]] ? results[arr[i]] = [i] : results[arr[i]].push(i);
+  }
+  return results;
+};
+
+console.log(letterPositions('hello low'));
