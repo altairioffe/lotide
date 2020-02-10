bcea5accc5ad0414dbfaae0535438ad18012f77c
Altair Ioffe <altair.ioffe@gmail.com>
2020-02-10 18:56:41 +0000

    added test for tail and refactored for export

--- tail.js
+++ tail.js
@@ -1,7 +1,5 @@
-const assertEqual = function(actual, expected) {
-  actual.every(x => x === expected[actual.indexOf(x)]) ? console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`) : console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
-};
+const assertEqual = require("./assertEqual")
 
 const tail = (arr) => arr.slice(1);
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]);
+
+module.exports = tail;
