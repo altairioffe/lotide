0cb35547037473e88ee97a28935507b47ca9b36b
Altair Ioffe <altair.ioffe@gmail.com>
2020-02-10 19:27:36 +0000

    created test for middle.js and made modular

--- middle.js
+++ middle.js
@@ -1,15 +1,5 @@
-
-const eqArrays = function(arr1, arr2) {
-  let match = true;
-  for (let i = 0; i < arr1.length; i++) {
-    arr1[i] !== arr2[i] ? match = false : '';
-  }
-  return match;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  actual === expected ? console.log(`🥳🥳🥳 Assertion Passed: ${actual} === ${expected}`) : console.log(`💩💩💩 Assertion Failed: ${actual} !== ${expected}`);
-};
+const eqArrays = require('./eqArrays')
+const assertArraysEqual = require('./assertArraysEqual')
 
 const middle = function(arr) {
   let result;
@@ -18,8 +8,5 @@ const middle = function(arr) {
   arr.length % 2 === 0 ? result = arr.slice(half - 1, half + 1) : result = arr[Math.floor(half)];
   return result;
 };
-//TEST CODE:
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4]), [2, 3]), true); //should PASS
-assertArraysEqual(eqArrays(middle([1, 3, 3, 4]), [2, 3]), false); //should FAIL
-assertArraysEqual(eqArrays(middle([1, 3, 3, 4]), [2, 3]), false); //should FAIL
-assertArraysEqual(eqArrays(middle([1, 2, 3, 4, 5]), [3]), true); //should PASS
+
+module.exports = middle;
